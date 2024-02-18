7f40fbb427ed4104ebb1d433dcf6810dfc05fe33
Volodymyr Vorobiov <funnyshotgun@gmail.com>
2024-02-18 23:27:40 +0100

    -Josfin Sans font included. -Added several dependencies: tailwindcss, postcss -The application layout has been partially created and stylized: Background with an image, Header with a logo and a theme switch button, an input with a checkbox to which the state has been added: on - off, content is centered using a container.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      // Добавляем пользовательский цвет фона
+      customBg: "hsl(235, 21%, 11%)",
+      customCl: "hsl(234, 39%, 85%)",
+      customSecBg: "hsl(235, 24%, 19%)",
+   
+    },
+    extend: {},
+  },
+  plugins: [],
+};
